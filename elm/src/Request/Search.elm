959308b341f4420a42b7436d74891032b52dd7e0
yannick-cw <yannick.gladow@googlemail.com>
2017-07-02 13:03:20 +0200

    Refactores Elm to use generated files.

--- elm/src/Request/Search.elm
+++ elm/src/Request/Search.elm
@@ -4,36 +4,11 @@ import Json.Decode exposing (field)
 import Json.Decode as Decode
 import Json.Encode as Encode
 import Http
-import Request.Tags exposing (encodeTags)
 import Model.Model exposing (..)
+import Generated.ParaApi exposing (postSearch)
+import Generated.Models exposing (Tags)
 
 
-search : List Tag -> Cmd Msg
+search : Tags -> Cmd Msg
 search tags =
-    let
-        url =
-            "/search"
-
-        body =
-            (encodeTags tags) |> Http.jsonBody
-
-        request =
-            Http.post url body decodeSearchResults
-    in
-        Http.send FetchedSearchResults request
-
-
-
-
-decodeSearchResults : Decode.Decoder (List SearchResult)
-decodeSearchResults =
-    Decode.at [ "results" ] (Decode.list decodesSearchResult)
-
-
-decodesSearchResult : Decode.Decoder SearchResult
-decodesSearchResult =
-    Decode.map3
-        SearchResult
-        (Decode.at [ "header" ] Decode.string)
-        (Decode.at [ "text" ] Decode.string)
-        (Decode.at [ "img" ] Decode.string)
+    Http.send FetchedSearchResults (postSearch tags)
