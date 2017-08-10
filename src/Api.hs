5313dd9b7243ed2489e48fb2f32a6e3fcae2d8aa
yannick-cw <yannick.gladow@googlemail.com>
2017-08-10 20:22:45 +0200

    Changes search results

--- src/Api.hs
+++ src/Api.hs
@@ -25,19 +25,35 @@ instance ElmType Tags
 
 instance FromJSON Tags
 
-newtype SearchResults = SearchResults
-  { results :: [SearchResult]
+data SearchResults = SearchResults
+  { searchRequest :: SearchRequest
+  , results       :: [SearchResult]
   } deriving (Generic)
+
+newtype SearchRequest = SearchRequest
+  { tag :: String
+  } deriving (Generic)
+
+instance FromJSON SearchRequest
+
+instance ToJSON SearchRequest
+
+instance ElmType SearchRequest
 
 instance ToJSON SearchResults
 
 instance ElmType SearchResults
 
+instance FromJSON SearchResults
+
 data SearchResult = SearchResult
-  { header :: String
-  , text   :: String
-  , img    :: String
+  { title       :: String
+  , imgSrc      :: String
+  , href        :: String
+  , description :: String
   } deriving (Generic)
+
+instance FromJSON SearchResult
 
 instance ToJSON SearchResult
 
@@ -51,9 +67,8 @@ instance ToJSON Saved
 
 instance ElmType Saved
 
-type ParaApi = "search" :> ReqBody '[ JSON] Tags :> Post '[ JSON] SearchResults
- :<|> "email" :> Capture "EMAIL" String :> Get '[ JSON] Tags
- :<|> "save" :> Capture "email" String :> ReqBody '[ JSON] Tags :> Post '[ JSON] Saved
+type ParaApi
+   = "search" :> ReqBody '[ JSON] Tags :> Post '[ JSON] [SearchResults] :<|> "email" :> Capture "EMAIL" String :> Get '[ JSON] Tags :<|> "save" :> Capture "email" String :> ReqBody '[ JSON] Tags :> Post '[ JSON] Saved
 
 type WebServiceApi = ParaApi :<|> Raw
 
--- src/ClientConnections.hs
+++ src/ClientConnections.hs
@@ -19,44 +19,28 @@ import           Servant.API
 import           Servant.Client
 import           Servant.Server             (Handler, ServantErr)
 
-searchForTags :: Tags -> Handler SearchResults
-searchForTags _ = return $ SearchResults [SearchResult "header" "text" "img.com"]
-
 tagsForEmail :: String -> Handler Tags
 tagsForEmail _ = return $ Tags ["tag1", "tag2"]
 
 saveTagsForEmail :: String -> Tags -> Handler Saved
 saveTagsForEmail _ _ = return $ Saved "ok"
 
-type SearchAPI = "xxx" :> Capture "query" String :> Get '[ JSON] SearchRes
+type ParaSearchApi = "search" :> ReqBody '[ JSON] SearchRequest :> Post '[ JSON] SearchResults
 
-api :: Proxy SearchAPI
+api :: Proxy ParaSearchApi
 api = Proxy
 
-search :: String -> ClientM SearchRes
+search :: SearchRequest -> ClientM SearchResults
 search = client api
 
-newtype SearchRes = SearchRes
-  { query :: String
-  } deriving (Generic, Show)
+fetchSearch :: Tags -> Handler [SearchResults]
+fetchSearch t = traverse (ExceptT . clientExecute . SearchRequest) (tags t)
 
-instance FromJSON SearchRes
+errTo :: SearchRequest -> ServantError -> Either ServantErr SearchResults
+errTo request _ = Right $ SearchResults request []
 
-queries :: ClientM SearchRes
-queries = search "mal"
-
--- prove of concept of using client library with server
-fetchTagsForEmail :: String -> Handler Tags
-fetchTagsForEmail _ = ExceptT $ fmap (either errTo rightTo) clientExecute
-
--- currently request is just prove of concept and gets replaced with tag result
-errTo :: ServantError -> Either ServantErr Tags
-errTo _ = Right $ Tags []
-
-rightTo :: SearchRes -> Either ServantErr Tags
-rightTo _ = Right $ Tags ["tag"]
-
-clientExecute :: IO (Either ServantError SearchRes)
-clientExecute = do
+clientExecute :: SearchRequest -> IO (Either ServantErr SearchResults)
+clientExecute request = do
   manager <- newManager tlsManagerSettings
-  runClientM queries (ClientEnv manager (BaseUrl Http "www.holidaycheck.de" 80 ""))
+  searchResOrError <- runClientM (search request) (ClientEnv manager (BaseUrl Http "localhost" 8082 ""))
+  return (either (errTo request) Right searchResOrError)
